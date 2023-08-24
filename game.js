ef8f7eb8e0069e350b897110f13e5208fd0efa2a
Jaspreet Singh <jaspreetsingh6881@gmail.com>
2023-08-24 21:23:13 +0530

    Add computer choice logic

--- game.js
+++ game.js
@@ -1,1 +1,10 @@
-console.log("Hello world")
+function getComputerChoice(){
+    let choice = Number.parseInt(Math.random()*3)+1;
+    if(choice==1){
+        return 'rock'
+    } else if(choice==2){
+        return 'paper'
+    }else{
+        return 'scissor'
+    }
+}
